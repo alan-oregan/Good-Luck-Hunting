91a5fdd81b35e49e600f1181ce704b76db7ebf1c
Tilka35 <B00133250@mytudublin.ie>
2021-11-11 12:49:26 +0000

    Player bounds (incomplete)
    Added player bounds code, not working.

---
+++ Good Luck Hunting/Assets/Scripts/PlayerBounds.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < ){
+            transform.position = new Vector3(/* COORD*/, transform.position.y, transform.position.z);
+        }
+    }
+}
