36afefe28459fd85d040dbf45aae7332927af653
Alan O'Regan <72257010+alan-oregan@users.noreply.github.com>
2021-11-25 21:26:14 +0000

    Made spawn interval a random value

--- Good Luck Hunting/Assets/Scripts/SpawnManager.cs
+++ Good Luck Hunting/Assets/Scripts/SpawnManager.cs
@@ -27,7 +27,7 @@ public class SpawnManager : MonoBehaviour
 
     IEnumerator SpawnTarget(){
         while(true){
-            yield return new WaitForSeconds(spawnInterval);
+            yield return new WaitForSeconds(Random.Range(.5f, spawnInterval));
             if (GameManager.isGameActive()) {
                 int index = Random.Range(0, duckPrefabs.Length);
                 Vector3 spawnPos = new Vector3(-25, Random.Range(1, 4), Random.Range(spawnRangeZLower, spawnRangeZHigher));
